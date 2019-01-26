1069af3e4508560ce378fc06f318c08f78bc8256
Christian McFarland <cmac@christians-mbp.lan>
2019-01-26 13:34:28 -0500

    add logic to create discount code in shopify

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,21 +6,42 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-// in latest body-parser use like below.
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// route handler for requests to /data
 app.post('/hook', (req, res, next) => {
 
-  console.log('stringified merges:::', JSON.stringify(req.body.data.merges));
-  console.log('not stringified merges:::', req.body.data.merges);
+  // Get data from new mailchimp subscriber and create the discoutn code string
+  const firstName = req.body.data.merges.FNAME;
+  const lastName = req.body.data.merges.FNAME;
+  const dicsountCode = `RAD_REP_${firstName.trim().replace(/\s/g, '')}_${lastName.trim().replace(/\s/g, '')}`;
 
-  res.status(200).send('OK')
+  // price rule id can be changed to offer different discount
+  const priceRuleId = '350241489007';
+
+  // post the new discount code to shopify
+  axios.post(
+    `https://surfpatch.myshopify.com/admin/price_rules/${priceRuleId}/discount_codes.json`,
+    {
+      discount_code: {
+        code: dicsountCode
+      }
+    },
+    {
+      auth: {
+        username: process.env.SHOPIFY_API_KEY || 'efcc1f9f2e74becd1ff3eebcac6d09aa',
+        password: process.env.SHOPIFY_SECRET || 'c4fe14341677c580282dd5e39e0e6480'
+    }
+   }).then(res => console.log(res));
+
+  res.status(200).send(`Code created: ${dicsountCode}`);
 });
 
-// route handler for requests to /data
 app.get('/hook', (req, res, next) => {
+  res.status(200).send("Server's up, dude!");
+});
+
+app.get('/', (req, res, next) => {
   res.status(200).send("Server's up, dude!");
 });
 
